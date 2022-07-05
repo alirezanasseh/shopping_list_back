32f4fab17fd1a113c2b8dbbb3212e32ba20e8ede
Alireza <alireza.nasseh@gmail.com>
2022-07-05 23:12:17 +0430

    Added user & item routes

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 import mongoose from 'mongoose';
+import {randomBytes} from 'crypto';
+import argon2 from 'argon2';
 
 mongoose.connect('mongodb://localhost:27017/shopping_list').then();
+
+interface IUser {
+    _id: string;
+    name: string;
+    email: string;
+    password: string;
+    salt: string;
+}
 
 interface IList {
     _id: string;
     name: string;
     order?: number;
+    user: string | IUser;
 }
 
 interface IItem {
@@ -17,12 +28,39 @@ interface IItem {
     bought: boolean;
 }
 
+const UserSchema = new mongoose.Schema<IUser>({
+    name: {
+        type: 'String',
+        required: true
+    },
+    email: {
+        type: 'String',
+        required: true
+    },
+    password: {
+        type: 'String',
+        required: true
+    },
+    salt: {
+        type: 'String',
+        required: true
+    },
+}, {
+    timestamps: true
+});
+const User = mongoose.model<IUser>('User', UserSchema);
+
 const ListSchema = new mongoose.Schema<IList>({
     name: {
         type: 'String',
         required: true
     },
-    order: Number
+    order: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 }, {
     timestamps: true
 });
@@ -84,6 +122,73 @@ app.put('/lists/:id', async (req: Request, res: Response) => {
 app.delete('/lists/:id', async (req: Request, res: Response) => {
     const result = await List.deleteOne({_id: req.params.id.toString()});
     res.json(result);
+});
+
+app.get('/lists/:listId/items', async (req: Request, res: Response) => {
+    const listId = req.params.listId;
+    if (listId) {
+        const items = await Item.find({list: listId.toString()});
+        res.json(items);
+    } else {
+        res.json({error: {message: 'List id should be provided'}});
+    }
+});
+
+app.post('/lists/:listId/items', async (req: Request, res: Response) => {
+    const listId = req.params.listId;
+    if (listId) {
+        const list = await List.findById(listId).lean();
+        if (list) {
+            const result = await Item.create({
+                list: listId,
+                ...req.body
+            });
+            res.json({status: !!result});
+        } else {
+            res.status(404).json({error: {message: 'List not found'}});
+        }
+    } else {
+        res.json({error: {message: 'List id should be provided'}});
+    }
+});
+
+app.post('/auth/register', async (req: Request, res: Response) => {
+    // req.body : {name, email, password}
+    let {name, email, password} = req.body;
+    // Checking for repeated email
+    const user = await User.find({email}).lean(); // {email: email}
+    if (user && user.length > 0) {
+        res.status(400).json({error: {message: 'Email is already registered'}});
+    } else {
+        // Hashing password
+        const argonSalt = randomBytes(32);
+        password = await argon2.hash(password, {salt: argonSalt});
+        const salt = argonSalt.toString('hex');
+
+        // Creating user
+        const newUser = await User.create({name, email, password, salt});
+
+        if (newUser) {
+            res.json({status: true, id: newUser._id});
+        } else {
+            res.status(500).json({error: {message: 'User not created'}});
+        }
+    }
+});
+
+app.post('/auth/login', async (req: Request, res: Response) => {
+    // req.body : {email, password}
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).lean();
+    if (user) {
+        if (await argon2.verify(user.password, password)) {
+            // JWT token generate
+        } else {
+            res.status(401).json({error: {message: 'Invalid credentials'}});
+        }
+    } else {
+        res.status(401).json({error: {message: 'Invalid credentials'}});
+    }
 });
 
 app.listen(port, () => {
